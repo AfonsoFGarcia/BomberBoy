32c56ffdd8af4258f4cea3608c32ff3efd206f66
Afonso Garcia <afonsogarcia@icloud.com>
2014-04-26 10:09:31 +0100

    Added robots

---
+++ ui/src/main/java/ist/cmov/proj/bomberboy/robots/Robot.java
@@ -0,0 +1,40 @@
+package ist.cmov.proj.bomberboy.robots;
+
+import ist.cmov.proj.bomberboy.status.GameStatus;
+
+/**
+ * Created by agfrg on 26/04/14.
+ */
+public class Robot extends Thread {
+
+    private static int SLEEPTIME = 1000;
+
+    private GameStatus status;
+    private Integer x;
+    private Integer y;
+    private Boolean dead;
+    private Boolean bomb;
+
+    public Robot(GameStatus status, Integer x, Integer y) {
+        this.status = status;
+        this.x = x;
+        this.y = y;
+        this.dead = false;
+        this.bomb = false;
+    }
+
+    @Override
+    public void run() {
+        while (!dead) {
+            try {
+                Thread.sleep(SLEEPTIME);
+            } catch (InterruptedException e) {
+                killRobot();
+            }
+        }
+    }
+
+    private void killRobot() {
+        this.dead = true;
+    }
+}
